bfbf7c1a82cb46126503c869eee124ace70f92c6
Shua <jketterl@gmail.com>
2021-06-11 23:29:05 -0700

    Fixed delayed throw bug

--- GMTK2021_game/Assets/Scripts/PickUpObjects.cs
+++ GMTK2021_game/Assets/Scripts/PickUpObjects.cs
@@ -47,23 +47,26 @@ public class PickUpObjects : MonoBehaviour
 
         RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.position, rayDirection * transform.localScale, rayDistance);
 
-        if (raycastHit2D.collider != null && raycastHit2D.collider.CompareTag("CanPickUp"))
-        {
-            if (holdButtonDown)
+        if (holdButtonDown)
+         {
+            if (raycastHit2D.collider != null && raycastHit2D.collider.CompareTag("CanPickUp"))
             {
                 raycastHit2D.collider.gameObject.transform.parent = holdPoint;
                 raycastHit2D.collider.gameObject.transform.position = holdPoint.position;
                 raycastHit2D.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-                holdButtonDown = false;
             }
-            else if (holdButtonUp)
+            holdButtonDown = false;
+         }
+         else if (holdButtonUp)
+         {
+            if (raycastHit2D.collider != null && raycastHit2D.collider.CompareTag("CanPickUp"))
             {
                 raycastHit2D.collider.gameObject.transform.parent = null;
                 raycastHit2D.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
                 raycastHit2D.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(1, 1) * throwVelocity;
-                holdButtonUp = false;
             }
-        }
+            holdButtonUp = false;
+         }
 
     }
 
