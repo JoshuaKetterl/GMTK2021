bf95a11860ff606b6f9724849d0121d04d5c69f5
Videt <egor310501@mail.ru>
2021-06-12 13:10:49 +1100

    movement and jumping

--- GMTK2021_game/Assets/Scripts/PlayerMovement.cs
+++ GMTK2021_game/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,109 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 movement;
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider2D;
+    private bool canJump;
+    private bool jumpButtonPressed;
+    [SerializeField] private float movementSpeed = 8f;
+    [SerializeField] private float jumpVelocity = 9f;
+    [SerializeField] private float fallGravity = 6f;
+    [SerializeField] private float lowJumpGravity = 5f;
+    [SerializeField] private LayerMask raycastLayerMask;
+
+    [SerializeField] private float coyoteTime = 0.1f;
+    private float coyoteTimeCounter;
+
+    [SerializeField] private float registerJumpBeforeLanding = 0.15f;
+    private float registerJumpBeforeLandingTimer;
+    private bool jumpAfterLanding;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpButtonPressed = true;
+            registerJumpBeforeLandingTimer = registerJumpBeforeLanding;
+        }
+        else
+        {
+            jumpButtonPressed = false;
+            registerJumpBeforeLandingTimer -= Time.deltaTime;
+        }
+
+        if (registerJumpBeforeLandingTimer > 0)
+            jumpAfterLanding = true;
+        else
+            jumpAfterLanding = false;
+
+        if (CheckGround())
+            coyoteTimeCounter = coyoteTime;
+        else
+            coyoteTimeCounter -= Time.deltaTime;
+
+        if ((CheckGround() && jumpButtonPressed) || (coyoteTimeCounter > 0 && jumpButtonPressed))
+            canJump = true;
+
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        transform.position += movementSpeed * Time.deltaTime * movement;
+
+        print(rb.gravityScale);
+    }
+
+    private void FixedUpdate()
+    {
+        if (canJump || (CheckGround() && jumpAfterLanding))
+        {
+            Jump();
+        }
+
+        if (canJump || CheckGround() || coyoteTimeCounter > 0)
+        {
+            rb.gravityScale = 1f;
+        }
+        else if (rb.velocity.y < jumpVelocity / 1.7)
+        {
+            rb.gravityScale = fallGravity;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.gravityScale = lowJumpGravity;
+        }
+        else rb.gravityScale = 1f;
+    }
+
+    private bool CheckGround()
+    {
+        float offsetBeyondPlayerCollider = 0.04f;
+        RaycastHit2D raycastHit2D = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, offsetBeyondPlayerCollider, raycastLayerMask);
+
+        Color rayColor;
+        if (raycastHit2D.collider != null)
+            rayColor = Color.green;
+        else
+            rayColor = Color.red;
+
+        Debug.DrawRay(boxCollider2D.bounds.center + new Vector3(boxCollider2D.bounds.extents.x, 0), Vector2.down * (boxCollider2D.bounds.extents.y + offsetBeyondPlayerCollider), rayColor);
+        Debug.DrawRay(boxCollider2D.bounds.center - new Vector3(boxCollider2D.bounds.extents.x, 0), Vector2.down * (boxCollider2D.bounds.extents.y + offsetBeyondPlayerCollider), rayColor);
+        Debug.DrawRay(boxCollider2D.bounds.center - new Vector3(boxCollider2D.bounds.extents.x, boxCollider2D.bounds.extents.y + offsetBeyondPlayerCollider), Vector2.right * boxCollider2D.bounds.extents.y, rayColor);
+        return raycastHit2D.collider != null;
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
+        canJump = false;
+        coyoteTimeCounter = 0;
+        //registerJumpBeforeLandingTimer = 0;
+        jumpAfterLanding = false;
     }
 }
