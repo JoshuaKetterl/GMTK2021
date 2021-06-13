cb47fd26972aadbd9a621405ae35c548bfeb0826
Shua <jketterl@gmail.com>
2021-06-13 09:29:15 -0700

    Fixed door sound spam

--- GMTK2021_game/Assets/Scripts/Node Variants/Door.cs
+++ GMTK2021_game/Assets/Scripts/Node Variants/Door.cs
@@ -10,18 +10,26 @@ public class Door : MonoBehaviour
     [FMODUnity.EventRef] public string DoorOpenEvent = "";
     [FMODUnity.EventRef] public string DoorCloseEvent = "";
 
+    bool open = false;
+
     // Update is called once per frame
     void Update()
     {
-        if (doorSwitchNode.GetOutput())
+        if (!open)
         {
-            animator.SetTrigger("Open");
-            FMODUnity.RuntimeManager.PlayOneShot(DoorOpenEvent, transform.position);
+            if (doorSwitchNode.GetOutput())
+            {
+                animator.SetTrigger("Open");
+                FMODUnity.RuntimeManager.PlayOneShot(DoorOpenEvent, transform.position);
+            }
         }
         else
         {
-            animator.SetTrigger("Close");
-            FMODUnity.RuntimeManager.PlayOneShot(DoorCloseEvent, transform.position);
+            if (!doorSwitchNode.GetOutput())
+            {
+                animator.SetTrigger("Close");
+                FMODUnity.RuntimeManager.PlayOneShot(DoorCloseEvent, transform.position);
+            }
         }
     }
 }
