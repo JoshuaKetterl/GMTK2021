092bcae92b445808979b731c6b0b4bc11d2c45aa
Shua <jketterl@gmail.com>
2021-06-13 00:13:21 -0700

    Decreased prox sensor range to 3

--- GMTK2021_game/Assets/Scripts/Node Variants/InputProximity.cs
+++ GMTK2021_game/Assets/Scripts/Node Variants/InputProximity.cs
@@ -6,7 +6,7 @@ public class InputProximity : OutputOnlyJoin
 {
 
     private GameObject player;
-    [SerializeField] float range = 5;
+    [SerializeField] float range = 3;
 
     void Start()
     {
